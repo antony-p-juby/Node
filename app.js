65407f6021e97c0d3d4daf7123daed450c433020
antony-p-juby <antonypjuby@gmail.com>
2025-08-07 16:28:17 +0530

    u r committed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express=require('express');
-
+const cors=require('cors');
 const body_parser=require('body-parser');
 const userRouter=require('./routers/user.router');
 const app=express();
+
+app.use(cors());
 app.use(body_parser.json());
 app.use('/',userRouter);
 
